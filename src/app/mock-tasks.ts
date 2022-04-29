f465d14a49eaa11ef7e9584e5bcd8d2399646d19
mirajans <miraj.ansari@sysfore.com>
2022-04-29 15:33:19 +0530

    mock task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+
+import {Task} from './Task'
+export const TASKS:Task[] =[
+  {
+    id:1,
+    text:'Meeting',
+    day:'may 10',
+    reminder:true,
+  },
+  {
+    id:2,
+    text:'Meeting with ceo',
+    day:'may 12',
+    reminder:true,
+  },
+  {
+    id:3,
+    text:'game',
+    day:'may 14',
+    reminder:true,
+  },
+  {
+    id:4,
+    text:'study',
+    day:'may 16',
+    reminder:true,
+  }
+]
